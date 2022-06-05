5d77ed4573d980d3d3eda00a00af141dc6eca829
Vladi <vladi85264@abv.bg>
2022-06-05 21:22:56 +0300

    updated gitignore

---
+++ BasketballTournamentSystem/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballTournamentSystem.Controllers
+{
+    public class SharedController : Controller
+    {
+        public IActionResult CustomErrorPage()
+        {
+            return View();
+        }
+    }
+}
