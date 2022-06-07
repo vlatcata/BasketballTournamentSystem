7ef11f09d978e61967671328eb0cacade150451f
Vladi <vladi85264@abv.bg>
2022-06-07 13:15:03 +0300

    Finished the layout of the frontend

---
+++ BasketballTournamentSystem/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using BasketballTournamentSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballTournamentSystem.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly IPlayerService playerService;
+
+        public PlayerController(IPlayerService _playerService)
+        {
+            playerService = _playerService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
